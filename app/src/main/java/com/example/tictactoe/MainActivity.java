c5821984ad44c2114118493238ba9ceb4bf0cf9b
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-11-03 12:01:42 -0800

    Added more game variables, an array to keep track of the status of the game, and a 2D array for keeping track of the winning combinations of squares.

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -11,6 +11,17 @@ public class MainActivity extends AppCompatActivity {
     private Button [] buttonArray = new Button[9];
     private Button resetGame;
 
+    private int p1Score, p2Score, squareCount;
+    boolean currentPlayer;
+
+    int [] gameStatus = {2,2,2,2,2,2,2,2};
+
+    int[][] winningRowsAndColumns = {
+            {0,1,2}, {3,4,5}, {6,7,8},//rows.
+            {0,3,6}, {1,4,7}, {2,5,8},//columns.
+            {0,4,8}, {2,4,6}//Diagonals
+    };
+
 
 
     @Override
