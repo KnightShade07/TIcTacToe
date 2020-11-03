4c35de9cbdcbb21c7a5fc8a54f4ee5296f4d2dc7
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-11-03 12:03:56 -0800

    Issue#1 on click function (#6)
    * Added basic game variables.
    
    * Added more game variables, an array to keep track of the status of the game, and a 2D array for keeping track of the winning combinations of squares.

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,8 +3,26 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView playerStatus;
+    private Button [] buttonArray = new Button[9];
+    private Button resetGame;
+
+    private int p1Score, p2Score, squareCount;
+    boolean currentPlayer;
+
+    int [] gameStatus = {2,2,2,2,2,2,2,2};
+
+    int[][] winningRowsAndColumns = {
+            {0,1,2}, {3,4,5}, {6,7,8},//rows.
+            {0,3,6}, {1,4,7}, {2,5,8},//columns.
+            {0,4,8}, {2,4,6}//Diagonals
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
