912bf54211471e4f09fa3d6e03d7ae9948484c40
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-11-03 11:50:18 -0800

    Added basic game variables.

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView playerStatus;
+    private Button [] buttonArray = new Button[9];
+    private Button resetGame;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
