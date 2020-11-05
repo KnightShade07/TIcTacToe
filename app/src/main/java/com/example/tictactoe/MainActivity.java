11a9764af78ecad11324c1e2a39beceb36032ef1
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-11-05 08:47:51 -0800

    Issue#19 final comments and tweaks (#36)
    * Ok, so apparently I had the view parameter accidently in the view itself and not on the button. I spent 3 days on this. 3 DAYS!!!!! Board now clears with just a click of a button. :)
    
    * Removed p1Score and p2Score variables, they were part of an optional score feature that I ultimately scrapped due to time constraints.
    
    * Added a few more documentation comments and cleaned up the spacing a bit!
    
    * Gave winningRowsAndColumns array a better name, since it includes diagonals, as well!
    
    * Removed any redundant casts or code.
    
    * Added a short comment for the btnCount variable.

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -13,38 +13,36 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView playerStatus;
     private Button [] buttonArray = new Button[10];
-    Button btnNewGame;
-    
-    private int p1Score, p2Score, btnCount;
+    private Button btnNewGame;
+    //This variable keeps track of the buttons and is
+    //incremented when a button is pressed.
+    //when btnCount = 9, the game will stop.
+    private int btnCount;
     boolean currentPlayer;
-
+    //This array keeps track of the empty, unmarked buttons.
     int [] gameStatus = {2,2,2,2,2,2,2,2,2,2};
-
-    int[][] winningRowsAndColumns = {
+    //2D array that will be used to keep track
+    //of the winning rows and columns for HW3.
+    int[][] winningCombinations = {
             {0,1,2}, {3,4,5}, {6,7,8},//rows.
             {0,3,6}, {1,4,7}, {2,5,8},//columns.
             {0,4,8}, {2,4,6}//Diagonals
     };
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        playerStatus = (TextView) findViewById(R.id.playerText);
-        btnNewGame = (Button) findViewById(R.id.btnNewGame);
+        playerStatus = findViewById(R.id.playerText);
+        btnNewGame = findViewById(R.id.btnNewGame);
         playerStatus.setText("Welcome to TicTacToe! To get started, click a square!");
         btnArrayClickListener();
-
-
     }
 
     @Override
     public void onClick(View v) {
         if (IsButtonPressed((Button) v)) return;
         changeTurns(v);
-
     }
 
     /**
@@ -55,14 +53,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         for (int i = 1; i < buttonArray.length; i++){
             String buttonID = "btn_" + i;
             int resourceID = getResources().getIdentifier(buttonID, "id", getPackageName());
-            buttonArray[i] = (Button) findViewById(resourceID);
+            buttonArray[i] = findViewById(resourceID);
             buttonArray[i].setOnClickListener(this);
         }
         btnCount = 0;
         currentPlayer = true;
     }
-
-
 
     /**
      * This method uses the currentPlayer boolean in order to change turns in games
@@ -73,7 +69,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     public void changeTurns(View v) {
         String btnID = v.getResources().getResourceEntryName((v.getId()));
-        int gameStatusTracker = Integer.parseInt(btnID.substring(btnID.length()-1, btnID.length()));
+        int gameStatusTracker = Integer.parseInt(btnID.substring(btnID.length()-1));
         if (currentPlayer){
             ((Button)v).setText("X");
             playerStatus.setText("It is now O's Turn.");
@@ -96,6 +92,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * @param v The button to be pressed.
      *
      */
+
     public boolean IsButtonPressed(Button v) {
         if (!v.getText().toString().equals("")){
             return true;
@@ -110,7 +107,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * The playerStatus.setText on line 119 makes sure that the playerText gets reset back to X
      * if the players decide to restart early for any reason.
      */
-    public void clearButtons(){
+    public void clearButtons(View v){
         btnCount = 0;
         currentPlayer = true;
         for (int i = 1; i < buttonArray.length; i++){
